715febe14218f7bda0bb6a489efa5396049ea990
Abel Quintanilla <abelqui15@gmail.com>
2022-04-21 14:46:02 -0500

    Chat Complete

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -3,9 +3,15 @@ import { onUnmounted, ref } from "vue";
 import useChat from "../composables/useChat";
 import useAuth from "../composables/useAuth";
 
-const { messages, unsubscribe } = useChat();
+const { messages, unsubscribe, sendMessage } = useChat();
+const { user } = useAuth();
 
 const newMessage = ref("");
+
+const send = () => {
+  sendMessage(newMessage.value);
+  newMessage.value = "";
+};
 
 onUnmounted(() => {
   unsubscribe();
@@ -14,16 +20,19 @@ onUnmounted(() => {
 
 <template>
   <h1 class="mt-8 text-6xl font-thin tracking-tighter text-center">
-    Cool Chat
+    Customer Service
   </h1>
   <div
     class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between"
   >
     <ul class="p-4 space-y-4">
       <li v-for="message in messages" :key="message.id">
-        <div class="flex justify-between px-4 py-2 bg-gray-200 rounded-lg">
+        <div
+          class="flex justify-between px-4 py-2 bg-gray-200 rounded-lg"
+          :class="user === message.author ? 'bg-blue-400' : 'bg-gray-200'"
+        >
           <span>{{ message.text }}</span>
-          <span> {{ message.author }}</span>
+          <span>by {{ message.author }}</span>
         </div>
       </li>
     </ul>
@@ -33,6 +42,7 @@ onUnmounted(() => {
         type="text"
         placeholder="Type a message..."
         v-model="newMessage"
+        @change="send"
       />
     </div>
   </div>
